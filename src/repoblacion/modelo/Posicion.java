f5cf557525c295d482cec4feddce342433f2cfa7
jmcab <jmcab@DESKTOP-BRH30L1>
2023-02-01 23:41:45 +0100

    creada clase posicion

--- src/repoblacion/modelo/Posicion.java
+++ src/repoblacion/modelo/Posicion.java
@@ -1,5 +1,50 @@
 package repoblacion.modelo;
 
-public class Posicion {
+import org.iesalandalus.programacion.utilidades.Entrada;
 
+public class Posicion {
+private double x;
+private double y;
+
+public Posicion (double x, double y) {
+	setX(x);
+	setY(y);
+}
+public Posicion (Posicion p) {
+	if (p == null)
+		throw new NullPointerException("ERROR: no es posible copiar una posicion nula");
+	setX ( p.getX());
+	setY ( p.getY());
+}
+
+public double getX() {
+	return x;
+}
+
+private void setX(double x) {
+	this.x = x;
+}
+
+public double getY() {
+	return y;
+}
+
+private void setY(double y) {
+	this.y = y;
+}
+public double distancia(Posicion posicion) {
+	double posElegida;
+	double distancia;
+	 posElegida = Entrada.realDoble();
+	if (posicion==null)
+		throw new NullPointerException("ERROR: la posicion no puede ser nula");
+	else
+	 distancia= posicion - posElegida;
+	return distancia;
+}
+
+@Override
+public String toString() {
+	return "Posicion [x=" + x  + ", y=" + y + "]";
+}
 }
