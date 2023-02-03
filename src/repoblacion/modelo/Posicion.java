6021a9a8a0a62aca9a8c7a4203ba67373463f3da
jmcab <jmcab@DESKTOP-BRH30L1>
2023-02-03 11:54:34 +0100

    reparada clase posicion

--- src/repoblacion/modelo/Posicion.java
+++ src/repoblacion/modelo/Posicion.java
@@ -1,6 +1,5 @@
 package repoblacion.modelo;
 
-import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Posicion {
 private double x;
@@ -33,14 +32,12 @@ private void setY(double y) {
 	this.y = y;
 }
 public double distancia(Posicion posicion) {
-	double posElegida;
-	double distancia;
-	 posElegida = Entrada.realDoble();
+	
 	if (posicion==null)
 		throw new NullPointerException("ERROR: la posicion no puede ser nula");
 	else
-	 distancia= posicion - posElegida;
-	return distancia;
+	 
+	return Math.sqrt(Math.pow(posicion.getX(), this.x) + Math.pow(posicion.getY(), this.y)) ;
 }
 
 @Override
